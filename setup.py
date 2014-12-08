a27cb2f45ce488282c7d0b05d48fb2a34b6b5e47
Xaver Y.R. Chen <yrchen@ATCity.org>
2014-12-08 23:17:27 +0800

    變更目錄結構與修正 setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+import os
 from setuptools import setup
+
+def package_data(pkg, root_list):
+    """Generic function to find package_data for `pkg` under `root`."""
+    data = []
+    for root in root_list:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
 
 setup(
     name='xblock-edx-pc2-judge',
@@ -11,4 +22,9 @@ setup(
             'edxpc2judge = edxpc2judge:EdxPc2JudgeBlock',
         ]
     }
+    packages=[
+        'edxpc2judge',
+    ],
+    include_package_data=True,
+    package_data=package_data("edxpc2judge", "static"),
 )
